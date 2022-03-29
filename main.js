2850c0fc1f02059df9162110f7bf52421e521d75
MGP-28 <joaosilvapereira96@hotmail.com>
2022-03-29 21:17:06 +0100

    simplified JS code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const audios = [
+const audios = [ //array with songs' data
     {
         name: "Cat Vibing",
         artist: "Ievan Polkka",
@@ -18,7 +18,7 @@ const audios = [
         uri: "https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3"
     }
 ];
-
+//global variable declaration here
 let index = 0, isActive = false;
 const songName = document.querySelector("#song-name");
 const artistName = document.querySelector("#artist-name");
@@ -28,17 +28,17 @@ const btnPlay = document.querySelector("#btnPlay");
 const btnMute = document.querySelector("#btnMute");
 const btnSoundOn = document.querySelector("#btnSoundOn");
 const progressBar = document.querySelector("#progressBar");
+//calling methods at start
 selectSong();
 loop();
-
+//background methods
 function loop(){
     if(isActive){
         const audio = document.getElementById("link-audio");
         const width = document.getElementById("progressBarBack").offsetWidth;
         console.log(width);
         const fracSongTime = linkAudio.currentTime / audios[index].duration;
-        const progBarWidth = "width:" + width*fracSongTime + "px";
-        progressBar.setAttribute("style", progBarWidth);
+        progressBar.setAttribute("style", `width: ${width*fracSongTime}px`);
         console.log(audio.currentTime); console.log(fracSongTime);
         if(width*fracSongTime >= width){
             forward();
@@ -46,19 +46,17 @@ function loop(){
     }
     setTimeout(loop, 500);
 }
-
 function selectSong(){
     songName.textContent = audios[index].name;
     artistName.textContent = audios[index].artist;
     linkAudio.src = audios[index].uri;
     linkAudio.load();
 }
-
+//methods triggered by buttons
 function play(){
     if(!isActive) {linkAudio.play(); isActive = true; btnPlay.classList.add("hidden"); btnPause.classList.remove("hidden")}
 	else {linkAudio.pause(); isActive = false; btnPlay.classList.remove("hidden"); btnPause.classList.add("hidden")}
 }
-
 function back(){
 	if(index > 0){ index--; }
     selectSong();
@@ -69,13 +67,11 @@ function forward(){
     selectSong();
     linkAudio.play();
 }
-
 function mute() {
     linkAudio.muted = true;
     btnMute.classList.remove("hidden");
     btnSoundOn.classList.add("hidden");
 }
-
 function soundOn() {
     linkAudio.muted = false;
     btnMute.classList.add("hidden");
