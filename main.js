f56b7391ddb99e01211febf490a7f59837e5b9ae
MGP-28 <joaosilvapereira96@hotmail.com>
2022-03-29 23:18:20 +0100

    Replay feature added when last song is played; IDs added to all buttons in HTML for future reference; methods for swapping buttons added; fixes for unwanted behaviour with play/pause

--- main.js
+++ main.js
@@ -19,12 +19,13 @@ const audios = [ //array with songs' data
     }
 ];
 //global variable declaration here
-let index = 0, isActive = false;
+let index = 0, isAudioActive = false, isReplayActive = false;
 const songName = document.querySelector("#song-name");
 const artistName = document.querySelector("#artist-name");
 const linkAudio = document.querySelector("#link-audio");
 const btnPause = document.querySelector("#btnPause");
 const btnPlay = document.querySelector("#btnPlay");
+const btnReplay = document.querySelector("#btnReplay");
 const btnMute = document.querySelector("#btnMute");
 const btnSoundOn = document.querySelector("#btnSoundOn");
 const progressBar = document.querySelector("#progressBar");
@@ -33,18 +34,11 @@ selectSong();
 loop();
 //background methods
 function loop(){
-    if(isActive){
-        const audio = document.getElementById("link-audio");
-        const width = document.getElementById("progressBarBack").offsetWidth;
-        console.log(width);
-        const fracSongTime = linkAudio.currentTime / audios[index].duration;
-        progressBar.setAttribute("style", `width: ${width*fracSongTime}px`);
-        console.log(audio.currentTime); console.log(fracSongTime);
-        if(width*fracSongTime >= width){
-            forward();
-        }
-    }
-    setTimeout(loop, 500);
+    const width = document.getElementById("progressBarBack").offsetWidth;
+    const fracSongTime = linkAudio.currentTime / audios[index].duration;
+    progressBar.setAttribute("style", `width: ${width*fracSongTime}px`);
+    if(width*fracSongTime >= width){ forward(); }
+    setTimeout(loop, 250);
 }
 function selectSong(){
     songName.textContent = audios[index].name;
@@ -52,28 +46,84 @@ function selectSong(){
     linkAudio.src = audios[index].uri;
     linkAudio.load();
 }
-//methods triggered by buttons
-function play(){
-    if(!isActive) {linkAudio.play(); isActive = true; btnPlay.classList.add("hidden"); btnPause.classList.remove("hidden")}
-	else {linkAudio.pause(); isActive = false; btnPlay.classList.remove("hidden"); btnPause.classList.add("hidden")}
+    //functions to show / hide buttons
+function btnReplayShowing(){
+    btnPlay.classList.add("hidden"); 
+    btnPause.classList.add("hidden"); 
+    btnReplay.classList.remove("hidden");
 }
-function back(){
-	if(index > 0){ index--; }
-    selectSong();
-    linkAudio.play();
+function btnPlayShowing(){
+    btnPlay.classList.remove("hidden"); 
+    btnPause.classList.add("hidden"); 
+    btnReplay.classList.add("hidden");
 }
-function forward(){
-	if(index < audios.length-1){ index++; }
-    selectSong();
-    linkAudio.play();
+function btnPauseShowing(){
+    btnPlay.classList.add("hidden"); 
+    btnPause.classList.remove("hidden"); 
+    btnReplay.classList.add("hidden");
 }
-function mute() {
-    linkAudio.muted = true;
+function btnMuteShowing(){
     btnMute.classList.remove("hidden");
     btnSoundOn.classList.add("hidden");
 }
-function soundOn() {
-    linkAudio.muted = false;
+function btnSoundOnShowing(){
     btnMute.classList.add("hidden");
     btnSoundOn.classList.remove("hidden");
+}
+//methods triggered by buttons
+function play(){
+    if(!isAudioActive) {
+        linkAudio.play(); 
+        isAudioActive = true; 
+        btnPauseShowing();
+    }
+	else {linkAudio.pause(); 
+        isAudioActive = false; 
+        btnPlayShowing();
+    }
+}
+function back(){
+    if(isReplayActive){
+        btnPlayShowing();
+        isReplayActive = false;
+    }
+    if(index > 0){ 
+        index--; 
+        if(isAudioActive){
+            linkAudio.play();
+        }
+    }
+    selectSong();
+}
+function forward(){
+	if(index < audios.length-1){ 
+        index++; 
+        selectSong(); 
+        if(isAudioActive){
+            linkAudio.play();
+        }
+    }
+    else{
+        linkAudio.pause();
+        linkAudio.currentTime = 0;
+        isAudioActive = false; 
+        isReplayActive = true;
+        btnReplayShowing();
+    }
+}
+function mute() {
+    linkAudio.muted = true;
+    btnMuteShowing();
+}
+function soundOn() {
+    linkAudio.muted = false;
+    btnSoundOnShowing()
+}
+function replay(){
+    index = 0; 
+    selectSong(); 
+    linkAudio.play(); 
+    isAudioActive = true;
+    isReplayActive = false;
+    btnPauseShowing();
 }
