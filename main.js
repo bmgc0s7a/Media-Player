f830fe913f0a2953124b867593f79da6de163f67
bmgcosta <bmgcosta@outlook.com>
2022-03-30 12:05:09 +0100

    Revert "Add rotation animation and fixed bug center CD"
    This reverts commit 9c7c1adf885b3fdc2a0152b68b921df21fe99bfc.

--- main.js
+++ main.js
@@ -3,21 +3,18 @@ const audios = [
         name: "Cat Vibing",
         artist: "Ievan Polkka",
         duration: 22,
-        img: "https://i.ytimg.com/vi/NUYvbT6vTPs/maxresdefault.jpg",
         uri: "audio/cat_vibing.mp3"
     },
     {
         name: "Creative Minds",
         artist: "Benjamin Tissot",
         duration: 147,
-        img: "https://www.bensound.com/bensound-img/creativeminds.jpg",
         uri: "https://www.bensound.com/bensound-music/bensound-creativeminds.mp3"
     },
     {
         name: "A New Beginning",
         artist: "Bensound",
         duration: 154,
-        img: "https://i1.sndcdn.com/artworks-000577426286-6p1jsj-t500x500.jpg",
         uri: "https://www.bensound.com/bensound-music/bensound-anewbeginning.mp3"
     }
 ];
@@ -31,7 +28,6 @@ const btnPlay = document.querySelector("#btnPlay");
 const btnMute = document.querySelector("#btnMute");
 const btnSoundOn = document.querySelector("#btnSoundOn");
 const progressBar = document.querySelector("#progressBar");
-const diskimg = document.querySelector('#discImg');
 selectSong();
 loop();
 
@@ -39,7 +35,6 @@ function loop(){
     if(isActive){
         const audio = document.getElementById("link-audio");
         const width = document.getElementById("progressBarBack").offsetWidth;
-        diskimg.classList.add("animation-rotate");
         console.log(width);
         const fracSongTime = linkAudio.currentTime / audios[index].duration;
         const progBarWidth = "width:" + width*fracSongTime + "px";
@@ -56,13 +51,12 @@ function selectSong(){
     songName.textContent = audios[index].name;
     artistName.textContent = audios[index].artist;
     linkAudio.src = audios[index].uri;
-    if(audios[index].img) diskimg.style.backgroundImage = `url(${audios[index].img})`;
     linkAudio.load();
 }
 
 function play(){
-    if(!isActive) {linkAudio.play(); isActive = true; btnPlay.classList.add("hidden"); btnPause.classList.remove("hidden");(diskimg.classList.contains('animation-rotate-pause')) ? diskimg.classList.remove('animation-rotate-pause') : ''}
-	else {linkAudio.pause(); isActive = false; btnPlay.classList.remove("hidden"); btnPause.classList.add("hidden");diskimg.classList.add("animation-rotate-pause")}
+    if(!isActive) {linkAudio.play(); isActive = true; btnPlay.classList.add("hidden"); btnPause.classList.remove("hidden")}
+	else {linkAudio.pause(); isActive = false; btnPlay.classList.remove("hidden"); btnPause.classList.add("hidden")}
 }
 
 function back(){
